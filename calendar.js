249f9de8b3dad5c1938a35f85bee3bd531cc944d
MartinRitsVOCO <martin.ritsberg@voco.ee>
2025-12-30 11:52:50 +0200

    first commit

---
+++ calendar.js
@@ -0,0 +1,18 @@
+const modal = document.getElementById("calendar-modal");
+const modalImage = document.getElementById("calendar-modal-image");
+const modalText = document.getElementById("calendar-modal-text");
+const calendarDays = document.getElementsByClassName("calendar-day");
+const imagePath = "images/days/";
+const imageExt = ".webp";
+
+function openDay() {
+  console.log(this.id);
+  const image = imagePath + this.dataset.image + imageExt;
+  modalImage.src = image;
+  modalText.textContent = this.dataset.text;
+  modal.showModal();
+}
+
+for (const day of calendarDays) {
+  day.addEventListener("click", openDay);
+}
